cf66147ebf24fa3ee2a6ffe2dc9953f6da571c9e
Anthony Alteirac <aalteirac@MacBook-Pro-4.local>
2020-09-18 23:08:28 +0200

    candidate

---
+++ help.js
@@ -0,0 +1,46 @@
+const h=`
+---------------FIRST FILL "store.js" INFORMATION TO CONNECT TABLEAU AND KEYCLOAK---------------
+
+----------------------------------------COMPARE USAGE------------------------------------------
+
+Run the following command to only compare Users (no modification in any repositories) :
+
+node index.js compare --NOCERT --reaml=myrealm
+
+--NOLOG                 Disable log message
+
+--NOCERT                Do not check ssl certificate validity
+
+--realm                 The KeyCloak realm name to compare
+
+------------------------------------------SYNC USAGE-------------------------------------------
+
+Run the following command to synchronize Users from KeyCloak to Tableau :
+
+node index.js sync --NOCERT --reaml=testsaml --defaultSiteRole=Viewer --defaultAuthSetting=ServerDefault
+
+--NOLOG                 Disable log message
+
+--NOCERT                Do not check ssl certificate validity
+
+--realm                 The KeyCloak realm name to compare
+
+--defaultSiteRole       Default Site Role assigned to new users created in Tableau 
+                            Value can be: 
+                                Viewer 
+                                Creator 
+                                Explorer 
+                                ExplorerCanPublish
+                                SiteAdministratorExplorer
+                                SiteAdministratorCreator 
+                                Unlicensed
+
+--defaultAuthSetting    Default Authentication Setting assigned to new users created in Tableau 
+                            Value can be: 
+                                ServerDefault 
+                                SAML 
+                                OpenID (Tableau Online only)
+`;
+module.exports = {
+    help_splash:h,
+}
