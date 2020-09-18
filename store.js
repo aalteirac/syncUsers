ea997dd86751156e09b09c479ef9398b1c703a05
Anthony Alteirac <anthony@alteirac.com>
2020-09-18 13:25:54 +0200

    Update store.js

--- store.js
+++ store.js
@@ -1,19 +1,19 @@
 module.exports = {
     TABLEAU:{
-        HOST:"<TABLEAU_HOST_NAME>", //Do not forget prot if not 80 or 443
+        HOST:"<TABLEAU_HOST_NAME>", //Do not forget port if not 80 or 443
         SITE_ID:"",
         API_ID:"<TOKEN_NAME>",
         API_TOKEN:"<TOKEN>",
         PROTOCOL:"https" //http or https
     },
     KEYCLOAK:{
-        HOST:"<KC_HOST_NAME>",
+        HOST:"<KC_HOST_NAME>", //Do not forget port if not 80 or 443
         CLIENT_ID:"<ID>",
         GRANT_TYPE:"password", //leave as it is
         CLIENT_SECRET:"<SECRET>",
         SCOPE:"openid",
         USER_NAME:"<USER_NAME>",
         PASS:"<PASSWORD>",
-        PROTOCOL:"http"
+        PROTOCOL:"http" //http or https
     }
 }
