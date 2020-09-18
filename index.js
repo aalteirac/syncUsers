d57cd1a3bb63f6e0e320f08c6b17e2322afcf385
Anthony Alteirac <aalteirac@MacBook-Pro-4.local>
2020-09-18 14:23:26 +0200

    add

--- index.js
+++ index.js
@@ -6,9 +6,31 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 async function compareRepo(){
     var tbu=await tab.getUsersList();
-    console.log(tbu);
     var kcu=await kc.getUsersList("testsaml");
-    console.log(kcu);
+    var toCreate=[];
+    var toDelete=[];
+    kcu.map((kcuser)=>{
+        var found=false;
+        tbu.map((tabuser)=>{
+            if(tabuser.name.toLowerCase()==kcuser.username.toLowerCase()){
+                found=true;
+            }
+        })
+        if(found===false)
+            toCreate.push(kcuser);
+    })
+    tbu.map((tabuser)=>{
+        var found=false;
+        kcu.map((kcuser)=>{
+            if(tabuser.name.toLowerCase()==kcuser.username.toLowerCase()){
+                found=true;
+            }
+        })
+        if(found===false)
+            toDelete.push(tabuser);
+    })
+    console.log("Exist in KC but not in Tableau",toCreate)
+    console.log("Exist in Tableau but not in KC",toDelete)
 }
 
 compareRepo();
