7f03b82ddc322b9129ad70541ac400384ed10ff2
aalteirac <anthony@alteirac.com>
2021-10-12 10:50:07 +0200

    init

--- store.js
+++ store.js
@@ -1,24 +1,17 @@
-export const TABLEAU = {
-    HOST: "10.177.51.176",
-    SITE_ID: "",
-    API_ID: "adm",
-    API_TOKEN: "zY0vYS0hTjCPWYrc8zNQKA==:9elIQziJshglsbXcvpThhVWWM9sW3s4i",
-    PROTOCOL: "https"
-};
-export const KEYCLOAK = {
-    HOST: "localhost:10002",
-    CLIENT_ID: "api",
-    GRANT_TYPE: "password",
-    CLIENT_SECRET: "ze0682d9e-49ed-4c74-95ed-a709b3719ca9",
-    SCOPE: "openid",
-    USER_NAME: "admin",
-    PASS: "secret",
-    PROTOCOL: "http"
-};
-// module.exports = {
-//     HOST:"10.177.48.89:8080",
-//     SITE_ID:"",
-//     API_ID:"adm",
-//     API_TOKEN:"29r//KRBTj65dsQWS/mCsA==:NAcSam2fSQVYwVybM1EjgY2TindVZ2Im",
-//     PROTOCOL:"http"
-// }
+export const TABLEAU={
+        HOST:"<TABLEAU_HOST_NAME>", //Do not forget prot if not 80 or 443
+        SITE_ID:"",
+        API_ID:"<TOKEN_NAME>",
+        API_TOKEN:"<TOKEN>",
+        PROTOCOL:"https" //http or https
+    },
+export const KEYCLOAK={
+        HOST:"<KC_HOST_NAME>",
+        CLIENT_ID:"<ID>",
+        GRANT_TYPE:"password", //leave as it is
+        CLIENT_SECRET:"<SECRET>",
+        SCOPE:"openid",
+        USER_NAME:"<USER_NAME>",
+        PASS:"<PASSWORD>",
+        PROTOCOL:"http"
+    }
